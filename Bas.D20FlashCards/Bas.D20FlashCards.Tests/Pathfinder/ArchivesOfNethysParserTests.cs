a1660d431b2aa067d99a673ad469a4546678a47a
Bas Paap <baspaap@live.nl>
2019-05-06 17:01:37 +0200

    Modify parser tests to use the new anmespace.

--- Bas.D20FlashCards/Bas.D20FlashCards.Tests/Pathfinder/ArchivesOfNethysParserTests.cs
+++ Bas.D20FlashCards/Bas.D20FlashCards.Tests/Pathfinder/ArchivesOfNethysParserTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Bas.D20FlashCards.Pathfinder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,30 @@ namespace Bas.D20FlashCards.Tests
     [TestClass]
     public sealed class ArchivesOfNethysParserTests
     {
-        private const string baseUri = "d20pfsrd.com";
+        private const string baseUri = "aonpd.com";
+
+        private const string archivesOfNethysBaseUri = "https://aonprd.com";
+
+        #region CanParse
+        [TestMethod]
+        public void CanParse_UriIsRelative_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act          
+            // Assert          
+            var exception = Assert.ThrowsException<ArgumentException>(() => ArchivesOfNethysParser.CanParse(new Uri(string.Empty), new Uri(archivesOfNethysBaseUri, UriKind.Absolute)));
+            Assert.AreEqual("uri", exception.ParamName);
+        }
+
+        [TestMethod]
+        public void CanPase_UriIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange
+            // Act          
+            // Assert          
+            var exception = Assert.ThrowsException<ArgumentNullException>(() => ArchivesOfNethysParser.CanParse(null, new Uri(archivesOfNethysBaseUri, UriKind.Absolute)));
+            Assert.AreEqual("uri", exception.ParamName);
+        }
+        #endregion
     }
 }
