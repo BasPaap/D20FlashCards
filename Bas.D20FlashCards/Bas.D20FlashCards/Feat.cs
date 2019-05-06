cfc243b383f6b1a7dbf982ccf1a84f6bc0b8236d
Bas Paap <baspaap@live.nl>
2019-05-06 13:41:49 +0200

    Implement derived card types.

--- Bas.D20FlashCards/Bas.D20FlashCards/Feat.cs
+++ Bas.D20FlashCards/Bas.D20FlashCards/Feat.cs
@@ -2,5 +2,9 @@
 {
     public sealed class Feat : Card
     {
+        public string Description { get; set; }
+        public string Flavor { get; set; }
+        public string Prerequisites { get; set; }
+        public string Normal { get; set; }
     }
 }
--- Bas.D20FlashCards/Bas.D20FlashCards/Skill.cs
+++ Bas.D20FlashCards/Bas.D20FlashCards/Skill.cs
@@ -2,5 +2,6 @@
 {
     public sealed class Skill : Card
     {
+        public string Description { get; set; }
     }
 }
