e9815a347c91c63c6c211c807aecd87a11dd815a
Bas Paap <baspaap@live.nl>
2019-05-08 11:56:56 +0200

    Test if index was found before continuing.

--- Bas.D20FlashCards/Bas.D20FlashCards/Extensions/StringExtensions.cs
+++ Bas.D20FlashCards/Bas.D20FlashCards/Extensions/StringExtensions.cs
@@ -10,9 +10,15 @@ namespace Bas.D20FlashCards.Extensions
         public static string Substring(this string fullString, string startString, string endString)
         {            
             var startIndex = fullString.IndexOf(startString);
+
+            if (startIndex < 0)
+            {
+                return null;
+            }
+
             var endIndex = fullString.Substring(startIndex).IndexOf(endString) + startIndex; // Make sure we take the -next- index of endString, after startString, and not just the first instance in the full string.
 
-            if (startIndex < 0 || endIndex == 0 || startIndex > endIndex)
+            if (endIndex == 0 || startIndex > endIndex)
             {
                 return null;
             }
